39c6c53610887c2ef2d2cf735ef23e2691d52c30
krak1 <karthikeyan.s.293@gmail.com>
2022-10-16 22:46:06 +0530

    connected flask app with html pages

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from email import contentmanager
 from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
@@ -5,6 +6,18 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/home")
+def demo2():
+    return render_template("demo2.html")
+
+@app.route("/chance")
+def chance(percent):
+    return render_template("chance.html", content=[percent])
+
+@app.route("/nochance")
+def no_chance(percent):
+    return render_template("noChance.html", content=[percent])
 
 @app.route('/<path:path>')
 def catch_all():
