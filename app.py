b54f3a16b807b8fbf32396bc0eaeedd261d52ce4
krak1 <karthikeyan.s.293@gmail.com>
2022-10-17 19:36:11 +0530

    connected api, flask routing, forms to html

--- app.py
+++ app.py
@@ -1,21 +1,49 @@
 from email import contentmanager
-from flask import Flask, render_template, redirect, url_for
+from http.client import responses
+import requests
+import pickle
+from flask import Flask, render_template, redirect, url_for, request
+
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods = ['POST', 'GET'])
 def index():
-    return render_template("index.html")
+    if request.method == 'POST':
+        
+        arr= [300,99,1,3,2,6.8,1]
+        # for i in request.form:
+        #     arr.append(float(request.form[i]))
+        
+        lab = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+        
+        args=""
+        for nu, la in zip(arr, lab):
+            args += la+'='+str(nu)+'&'
+
+        print('\n\nhttp://usouthcloudibm.pythonanywhere.com/logistic?'+args[:-1])
+        response = requests.get('http://usouthcloudibm.pythonanywhere.com/logistic?'+args[:-1])
+        res = response.json()
+
+        percent = pickle.load(open('university_percent.pkl', 'rb')).predict([arr])
+        print(percent)
+        print(res)
+        if res['status'] == 'True':
+            return redirect(url_for('chance', percent=percent[0]))
+        else:
+            return redirect(url_for('chance', percent=percent[0]))
+    else:
+        return render_template("index.html")
 
 @app.route("/home")
 def demo2():
     return render_template("demo2.html")
 
-@app.route("/chance")
+@app.route("/chance/<percent>")
 def chance(percent):
     return render_template("chance.html", content=[percent])
 
-@app.route("/nochance")
+@app.route("/nochance/<percent>")
 def no_chance(percent):
     return render_template("noChance.html", content=[percent])
 
@@ -24,4 +52,4 @@ def catch_all():
     return redirect(url_for("index"))
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
